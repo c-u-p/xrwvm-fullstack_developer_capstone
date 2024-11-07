82d57aac9ec4c8443463478b1d275b833654cecd
Umesh Padul <umeshcpadul@gmail.com>
2024-11-07 16:33:08 +0530

    Update populate.py for linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -14,7 +14,8 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(name=data['name'],
                                                          description=data['description']
-                                                        ))
+                                                        )
+                                 )
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -33,7 +34,6 @@ def initiate():
       {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
