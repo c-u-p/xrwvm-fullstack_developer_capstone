533a01fad158e13e1da909244cc29eaf9fd856b6
Umesh Padul <umeshcpadul@gmail.com>
2024-11-07 17:30:36 +0530

    Update settings.py for linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['localhost',
                  'https://umeshpadul2-8000.\
                  theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.\
                  cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://umeshpadul2-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://umeshpadul2-8000.\
+theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
